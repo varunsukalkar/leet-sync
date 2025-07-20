8464334e8258dfa32698c8cd2ea51745ebf0c586
varun sukalkar <92172651+varunsukalkar@users.noreply.github.com>
2025-07-20 13:00:13 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (24.28%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,26 +1,41 @@
 class Solution {
 public:
     int myAtoi(string s) {
-     int i = 0, n = s.size();
-    while (i < n && s[i] == ' ') i++;  // Skip leading spaces
+        int n = s.size();
+        int i = 0 ;
+        long long number = 0 ;
+        int sign = 1;
+        while(i< n && s[i]==' ' ){
+           
+          i++;
+        }
+        if(i< n && (s[i]=='+' || s[i]=='-')){
+            sign = (s[i]=='-') ? -1 : 1;
+            i++;
+        }
 
-    int sign = 1;
-    if (i < n && (s[i] == '+' || s[i] == '-')) {
-        sign = (s[i] == '-') ? -1 : 1;
-        i++;
-    }
+         
 
-    long long number = 0;
-    while (i < n && isdigit(s[i])) {
-        number = number * 10 + (s[i] - '0');
+        while(i< n && isdigit(s[i])){
+            number = number * 10 + (s[i]-'0');
+            if(sign == 1 &&  number>INT_MAX) return INT_MAX;
+            if(sign == -1 && -number<INT_MIN)return INT_MIN;
+            i++;
+        }
+        return sign * number ; 
+
+
+
+
+
+
+
+
+
+
+
+
+
         
-        // Clamp if out of int range
-        if (sign == 1 && number > INT_MAX) return INT_MAX;
-        if (sign == -1 && -number < INT_MIN) return INT_MIN;
-
-        i++;
-    }
-
-    return sign * number;
     }
 };
